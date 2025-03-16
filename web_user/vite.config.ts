23fbc83188c75c7ddaf96530f96875f147cad130
feierman <1981879111@qq.com>
2025-03-16 23:55:11 +0800

    搭建项目

--- web_user/vite.config.ts
+++ web_user/vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+// vite.config.ts
+import { defineConfig } from 'vite'     //导入vite
+import vue from '@vitejs/plugin-vue'     //导入vue
+import path from 'path'                 //导入path
+import AutoImport from 'unplugin-auto-import/vite'  //导入unplugin-auto-import
+import Components from 'unplugin-vue-components/vite'   //导入unplugin-vue-components
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'   //导入ElementPlusResolver
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue(),
+        AutoImport({
+            imports: ['vue', 'vue-router'],
+            dts: true,
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+            dts:'src/auto-imports.d.ts'
+        }),
+    ],
+    // 别名
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+    //全局scss
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+                @use "@/assets/style/variable.scss" as *;
+            `,
+            },
+        },
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    }
 })
