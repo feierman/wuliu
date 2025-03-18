ca8056912c39092de34ca9d036ba6f7e39bbb5f8
Feierman <84377558+feierman@users.noreply.github.com>
2025-03-18 17:40:35 +0800

    Merge branch 'main' of github.com:feierman/wuliu
    # Conflicts:
    #	.idea/JavaScriptSceneConfigState.xml
    #	web_user/components.d.ts
    #	web_user/src/layouts/Defaultlayout.vue

--- web_user/src/components/Pcheader.vue
+++ web_user/src/components/Pcheader.vue
@@ -1,70 +1,99 @@
 <template>
-  <a-row>
-    <a-col :span="24" class="top">
-      <router-link to="/">
-        <a-image
-            :width="60"
-            :preview="false"
-            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-            style="border-radius: 10px"
-        />
+  <a-row  class="top" >
+    <a-col class="left" :span="6">
+      <router-link  to="/">
+        <a-image :width="60" :preview="false" :src="href" style="border-radius: 10px" />
       </router-link>
-      <div class="right">
-        <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
-
-      </div>
     </a-col>
+      <a-col class="right" :span="18" >
+        <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" @click="handleMenuClick" />
+      </a-col>
+
   </a-row>
 
 </template>
+
 <script lang="ts" setup>
 import { h, ref } from 'vue';
-import { MailOutlined, SettingOutlined } from '@ant-design/icons-vue';
 import { MenuProps } from 'ant-design-vue';
-const current = ref<string[]>(['mail']);
+import { useRouter } from 'vue-router';
+const href = ref<string>('https://aliyuncdn.antdv.com/vue.png');
+const router = useRouter();
+const current = ref<string[]>(['1']);
+
 const items = ref<MenuProps['items']>([
-    {
-        key: 'home',
-        icon: () => h(MailOutlined),
-        label: '首页',
-
-    },
-    {
-        key: 'utools',
-        icon: () => h(SettingOutlined),
-        label: '行业工具',
-
-        children: [
-            {
-              key:'xinxiadd',
-                label: '信息发布 ',
-
-            },
-            {
-              key:'dingdanSearch',
-                label: '订单查询',
-
-            },
-        ],
-    },
   {
-    key:'shop',
-    label:'商家入驻',
-    title:'shop'
+    key: '1',
+    icon: () => h('i', { class: 'iconfont icon-wuliugongsi' }), // 使用 iconfont
+    label: '首页',
+    title: '首页',
   },
   {
-    key:'logo',
-    label:'登录',
-    title:'login'
-  }
-
+    key: '2',
+    icon: () => h('i', { class: 'iconfont icon-gengduo3' }), // 使用 iconfont
+    label: '行业工具',
+    title: '行业工具',
+    children: [
+      {
+        key: '2-1',
+        icon: () => h('i', { class: 'iconfont icon-xinxifabu' }), // 使用 iconfont
+        label: '信息发布',
+        title: '信息发布',
+      },
+      {
+        key: '2-2',
+        icon: () => h('i', { class: 'iconfont icon-dingdanchaxun' }), // 使用 iconfont
+        label: '订单查询',
+        title: '订单查询',
+      },
+    ],
+  },
+  {
+    key: '3',
+    icon: () => h('i', { class: 'iconfont icon-shangjiaruzhu' }), // 使用 iconfont
+    label: '商家入驻',
+    title: '商家入驻',
+  },
+  {
+    key: '4',
+    icon: () => h('i', { class: 'iconfont icon-denglu-copy' }), // 使用 iconfont
+    label: '登录',
+    title: '登录',
+  },
 ]);
-</script>
 
-<style scoped lang="scss" >
-  .top{
-    display: flex !important;
-    justify-content: space-between;
-    align-items: center;
+const handleMenuClick = ({ key }: { key: string }) => {
+  console.log('Clicked menu item:', key);
+  current.value = [key];
+
+  if (key === '1') {
+    router.push('/');
+  } else if (key === '2-1') {
+    router.push('/xinxiadd');
+  } else if (key === '2-2') {
+    router.push('/dingdansearch');
+  } else if (key === '3') {
+    router.push('/shop');
+  } else if (key === '4') {
+    router.push('/login');
   }
+};
+</script>
+<style scoped lang="scss">
+.top{
+  //background-color: $color-surface;
+  width: 100%;
+  height: 60px;
+}
+
+.right{
+  display: flex;
+  justify-content: flex-end;
+  i{
+    margin-right: 8px;
+  }
+}
+.ant-menu {
+  border-bottom: none !important; /* 移除底部边框 */
+}
 </style>
